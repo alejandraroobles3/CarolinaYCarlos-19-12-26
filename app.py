bd84e2b72efa5ce8c0b6edafed0eac21087fdc68
Alejandra Robles M <64538989+alejandraroobles3@users.noreply.github.com>
2026-01-16 13:26:09 -0600

    changes

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, redirect, url_for
 import time
+import os
+from pydrive2.auth import GoogleAuth
+from pydrive2.drive import GoogleDrive
+import gspread
+from oauth2client.service_acount import ServiceAccountCredentials
+
 #para activar entorno virtual es:
 #venv\bin\activate.bat
 #y luego python app.py
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER']='uploads'
+
+gauth=
 
 @app.route("/")
 def index():
@@ -28,6 +37,7 @@ def pagina():
     #aqui ponemos la carta que se abre? 
     if request.method == "POST":
         return render_template("index.html")
-
+    elif request.method == "GET":
+        return render_template("index.html")
 if __name__ == "__main__":
     app.run(debug=True)
