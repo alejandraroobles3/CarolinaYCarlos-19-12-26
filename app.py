3e9efedf748b16a628e9547f094c4e46bf03e400
Alejandra Robles M <64538989+alejandraroobles3@users.noreply.github.com>
2026-01-21 18:40:44 -0600

    listo

--- app.py
+++ app.py
@@ -156,4 +156,5 @@ def enviar():
         return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run(debug=True, use_reloader=False)
+    port = int(os.environ.get("PORT",5000))
+    app.run(host="0.0.0.0", use_reloader=False, port=port)
